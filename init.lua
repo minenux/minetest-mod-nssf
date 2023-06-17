5ad71feb22ab6c983220ea51759c946f519a4aa8
mckaygerhard <mckaygerhard@gmail.com>
2023-06-16 23:09:43 -0400

    fix loading incorrect function name

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 nssf = {}
 local mod = nssf
 
-mod.version = '20180720'
+mod.version = '20230616'
 mod.mod_name = minetest.get_current_modname()
 mod.path = minetest.get_modpath(mod.mod_name)
 mod.world = minetest.get_worldpath()
